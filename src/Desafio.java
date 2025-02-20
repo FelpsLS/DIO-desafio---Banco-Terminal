f9b7b1ae6c321b219613662afb24b81137c370df
Fellipe Lopes Silva <fellipelopessilva5@gmail.com>
2025-02-20 10:54:05 -0300

    Desafio - DIO

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,4 +1,6 @@
-public class App {
+
+public class Desafio {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Por favor, digite o número da Conta !");
         int numbCount = Integer.valueOf(args[0]);
@@ -9,6 +11,6 @@ public class App {
         System.out.println("Por favor, digite o saldo inicial da conta !");
         Double saldo = Double.valueOf(args[3]);
 
-        System.out.println("Olá "+clientName+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numbCount+" e seu saldo "+saldo+" já está disponível para saque");
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numbCount + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
